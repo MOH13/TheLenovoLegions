8b399cad8dda855a6d72efed6fef3e3ec2c54804
Viktor Salling <vik.salling@hotmail.com>
2023-09-19 15:07:15 +0200

    no double jump

--- Assets/CatBehaviour.cs
+++ Assets/CatBehaviour.cs
@@ -12,29 +12,63 @@ public class CatBehaviour : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rigidBody;
+    private bool onGround;
 
     MyPlayerInput input;
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnEnable()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
-        speed = 5;
+        input.Player.Enable();
+    }
+    private void OnDisable()
+    {
+        input.Player.Disable();
+    }
+
+    private void Awake()
+    {
         input = new MyPlayerInput();
-        input.Enable();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground")) {
+            onGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground")) {
+            onGround = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground")) {
+            onGround = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        speed = 5;
     }
 
 
     // Update is called once per frame
-    void Update()
-    {
-        if (input.Player.Jump.WasPressedThisFrame())
-        {
+    void Update() {
+        if (input.Player.Jump.WasPressedThisFrame() && onGround) {
             rigidBody.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
         }
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
+        if (input.Player.Dive.IsPressed()) {
+            rigidBody.AddForce(Vector2.down * (speed / 10), ForceMode2D.Impulse);
+        }
         var moveDir = input.Player.Move.ReadValue<float>();
         rigidBody.AddForce(Vector2.right * speed * moveDir * (Time.deltaTime * 60));
     }
