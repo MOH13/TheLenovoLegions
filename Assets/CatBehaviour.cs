e53159ffc9fc3435a4678c7eb39fb83e7ed4d783
Viktor Salling <vik.salling@hotmail.com>
2023-10-03 14:30:53 +0200

    proper ground detection

--- Assets/CatBehaviour.cs
+++ Assets/CatBehaviour.cs
@@ -12,7 +12,8 @@ public class CatBehaviour : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rigidBody;
-    private bool onGround;
+    [SerializeField] private LayerMask platformLayerMask;
+    public BoxCollider2D boxCollider2d;
     public AudioSource catWalk;
     public AudioSource catMeow;
     public AudioSource catRun;
@@ -32,63 +33,61 @@ public class CatBehaviour : MonoBehaviour
     {
         input = new MyPlayerInput();
         rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Ground")) { // Should improve this, so we only set onGround when cat is on ground and not just touching an object
-            onGround = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Ground")) {
-            onGround = false;
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Ground")) {
-            onGround = true;
-        }
-    }
-
-    // Start is called before the first frame update
     void Start() {
         speed = 5;
     }
 
-
-    // Update is called once per frame
     void Update() {
-        if (input.Player.Jump.WasPressedThisFrame() && onGround) {
+        if (input.Player.Jump.WasPressedThisFrame() && isGrounded()) {
             rigidBody.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
             catWalk.enabled = false;
         }
     }
 
     private void FixedUpdate() {
+        var moveDir = input.Player.Move.ReadValue<float>();
+        bool isShiftPressed = input.Player.Shift.ReadValue<float>() == 1;
+        handleMovementSounds(isShiftPressed, moveDir);
+        handleMovement(isShiftPressed, moveDir);
+    }
+
+    private bool isGrounded() {
+        float extraHeight = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size - new Vector3(0.1f, 0.1f, 0f), 0f, Vector2.down, extraHeight, platformLayerMask);
+        return raycastHit.collider != null;
+    }
+
+    private void handleMovement(bool isShiftPressed, float moveDir)
+    {
         if (input.Player.Dive.IsPressed()) {
             rigidBody.AddForce(Vector2.down * (speed / 10), ForceMode2D.Impulse);
         }
-        var moveDir = input.Player.Move.ReadValue<float>();
-        bool isShiftPressed = input.Player.Shift.ReadValue<float>() == 1;
-        if (moveDir != 0 && onGround) {
-            if (isShiftPressed) {
+        var sprintMultiplier = isShiftPressed ? 1.3f : 1; // Maybe a variable or something
+        rigidBody.AddForce(Vector2.right * speed * sprintMultiplier * moveDir * (Time.deltaTime * 60));
+    }
+
+    private void handleMovementSounds(bool isShiftPressed, float moveDir)
+    {
+        if (moveDir != 0 && isGrounded())
+        {
+            if (isShiftPressed)
+            {
                 catRun.enabled = true;
                 catWalk.enabled = false;
             }
-            else {
+            else
+            {
                 catWalk.enabled = true;
                 catRun.enabled = false;
             }
-        } else {
+        }
+        else
+        {
             catWalk.enabled = false;
             catRun.enabled = false;
         }
-        var sprintMultiplier = isShiftPressed ? 1.3f : 1; // Maybe a variable or something
-        rigidBody.AddForce(Vector2.right * speed * sprintMultiplier * moveDir * (Time.deltaTime * 60));
     }
 }
